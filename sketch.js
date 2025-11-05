41c0b29c7ea177c8c8b8ab793890c27dc688a933
dantheman872 <95766593+dantheman872@users.noreply.github.com>
2025-11-05 00:34:18 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let yPosition = []
 let diameter = []
 let density = []
 let ySpeed = []
+let xSpeed = []
+let ballNo = 50
 let gravityAcceleration = 0.4
-
-
 
 function setup() {
 
@@ -16,10 +16,12 @@ function draw() {
 
     background(220)   
 
-    for(let i = 0; i < ySpeed.length; i++){
+    for(let i = 0; i < xPosition.length; i++){
 
         ySpeed[i] += gravityAcceleration
         yPosition[i] += ySpeed[i]
+        xPosition[i] += xSpeed[i]
+        
 
         if(yPosition[i] + diameter[i]/2 > height){
 
@@ -28,7 +30,29 @@ function draw() {
             yPosition[i] = height - diameter[i]/2
         }
 
+        if(yPosition[i] - diameter[i]/2 < 0){
+
+            ySpeed[i] = -ySpeed[i]
+            ySpeed[i] = ySpeed[i] / density[i]
+            yPosition[i] = 0 + diameter[i]/2
+        }
+
+        if(xPosition[i] + diameter[i]/2 > width){
+
+            xSpeed[i] = -xSpeed[i]
+            xSpeed[i] = xSpeed[i] / density[i]
+            xPosition[i] = height - diameter[i]/2
+        }
+
+        if(xPosition[i] - diameter[i]/2 < 0){
+
+            xSpeed[i] = -xSpeed[i]
+            xSpeed[i] = xSpeed[i] / density[i]
+            xPosition[i] = 0 + diameter[i]/2
+        }
+
         ySpeed[i] = ySpeed[i] * 0.997
+        xSpeed[i] = xSpeed[i] * 0.997
 
         fill(200,0,0)
         ellipse(xPosition[i], yPosition[i], diameter[i], diameter[i])
@@ -37,9 +61,18 @@ function draw() {
 
 function mousePressed(){
 
-    xPosition.push(mouseX)
-    yPosition.push(mouseY)
-    ySpeed.push(0)
+    for(let p = 0; p < ballNo; p ++){
+
+        spawnCircle(mouseX, mouseY, random(-10, 10), random(-10, 10))        
+    }    
+}
+
+function spawnCircle(x, y, yDir, xDir){
+
+    xPosition.push(x)
+    yPosition.push(y)
+    xSpeed.push(xDir)
+    ySpeed.push(yDir)
     density.push(1.25 + random(0.01,0.1))
-    diameter.push(20 + random(-5,5))
+    diameter.push(15 + random(-5,5))
 }
